021808b14803e6f716cc11a094c66cfbbe1aa8a1
flooood <viktordow@hotmail.com>
2017-11-20 14:51:26 +0100

    simple index file with css and database tables

--- website/settings.py
+++ website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'demo.apps.DemoConfig',
     'music.apps.MusicConfig',
     'django.contrib.admin',
     'django.contrib.auth',
