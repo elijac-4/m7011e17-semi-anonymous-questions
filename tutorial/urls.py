669a01a459e3772ac789358c3cf549eae0fbc493
Elis Jacobson <Jacobson.Elis@gmail.com>
2017-12-02 21:36:41 +0100

    initial commit

---
+++ tutorial/urls.py
@@ -0,0 +1,14 @@
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf.urls import include
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('snippets.urls')),
+]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
