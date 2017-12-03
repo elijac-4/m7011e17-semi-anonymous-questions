9d4770a2d1b077ddcfb64c85f44e8fe3cbeccbfc
Elis Jacobson <Jacobson.Elis@gmail.com>
2017-12-03 16:11:42 +0100

    initial commit. basic rest + user registration

---
+++ start/serializers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+from django.contrib.auth.hashers import make_password
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class UserSerializer2(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=make_password(validated_data['password'])
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
---
+++ start/views.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer, UserSerializer2
+
+# For HTML Pages
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from rest_framework.permissions import *
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = (AllowAny,)
+
+        return super(UserViewSet, self).get_permissions()
+
+
+class UserViewSet2(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer2
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = (AllowAny,)
+
+        return super(UserViewSet2, self).get_permissions()
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class Index(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'start/index.html'
+
+    def get(self, request):
+        queryset = User.objects.all().order_by('-date_joined')
+        return Response({'users': queryset})
